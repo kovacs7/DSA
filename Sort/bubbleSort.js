005e7184f255ccfed56e78cadc487f59b1d80ea8
kovacs7 <asishkumarbehera.official@gmail.com>
2024-07-11 23:42:00 +0530

    initial commit

---
+++ Sort/bubbleSort.js
@@ -0,0 +1,24 @@
+
+// https://btholt.github.io/complete-intro-to-computer-science/bubble-sort/
+/*
+worst & average case time - O(n2) | best case sorted list O(n)
+space-O(1)
+*/
+
+function bubbleSort(nums) {
+  let swapped = false;
+  do {
+    swapped = false;
+    let iterations = 0;
+    for (let i = 0; i < nums.length  - iterations; i++) {
+      if (nums[i] > nums[i + 1]) {
+        const temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+        ++iterations;
+      }
+    }
+  } while (swapped);
+  return nums
+}
---
+++ Sort/insertionSort.js
@@ -0,0 +1,43 @@
+// https://btholt.github.io/complete-intro-to-computer-science/insertion-sort/
+// There are other fancy ways as well.
+
+
+/*
+worst & average case time - O(n2) | best case sorted list O(n2)
+space-O(1)
+*/
+
+function insertionSort(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let count = 0;
+    for (let j = i - 1; j >= 0; j--) { 
+      if (nums[j] > nums[i - count]) {
+        let temp = nums[i - count];
+        nums[i - count] = nums[j];
+        nums[j] = temp;
+      }
+      ++count;
+    }
+  }
+  return nums;
+}
+
+
+
+/*
+worst & average case time - O(n2) | best case sorted list O(n)
+space-O(1)
+*/
+
+function insertionSort(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let count = 0;
+    for (let j = i - 1; nums[j] > nums[i - count] && j >= 0; j--) { // see the changes here
+      let temp = nums[i - count];
+      nums[i - count] = nums[j];
+      nums[j] = temp;
+      ++count;
+    }
+  }
+  return nums;
+}
